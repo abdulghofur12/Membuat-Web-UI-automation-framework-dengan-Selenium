b967076cd12554822d1963415a65aeadec50abcd
abdulghofur12 <ghofursans12@gmail.com>
2025-02-20 19:05:08 +0700

    POM

--- src/test/java/ghofur/stepdef/LoginStepDef.java
+++ src/test/java/ghofur/stepdef/LoginStepDef.java
@@ -1,69 +1,35 @@
 package ghofur.stepdef;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import ghofur.stepdef.page.LoginPage;
+import ghofur.stepdef.BaseTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class LoginStepDef {
-
-    WebDriver driver;
-
-    @Before
-    public void beforeTest(){
-        driver = WebDriverManager.chromedriver().create();
-    }
-    @After
-    public void  AfterTest(){
-        driver.close();
-
-    }
+public class LoginStepDef extends BaseTest {
+     LoginPage loginPage;
 
     @Given("user is on login page")
     public void userIsOnLoginPage() {
-        driver.get("https://www.saucedemo.com/");
+       loginPage = new LoginPage(driver);
+       loginPage.goToLoginPage();
     }
-
     @And("user input username with {string}")
     public void userInputUsernameWith(String username) {
-        By usernameInputText = By.cssSelector("input#user-name");
-        driver.findElement(usernameInputText).sendKeys(username);
-
+        loginPage.inputUsername(username);
 
     }
-
     @And("user input password with {string}")
     public void userInputPasswordWith(String password) {
-        By passwordInputText = By.xpath("//*[@id=\"password\"]");
-        driver.findElement(passwordInputText).sendKeys(password);
+       loginPage.inputPassword(password);
     }
-
     @When("user click login button")
     public void userClickLoginButton() {
-        By loginButton = By.id("login-button");
-        driver.findElement(loginButton).click();
+        loginPage.clickLoginButton();
     }
-
-
-    @Then("user is on homepage")
-    public void userIsOnHomepage() {
-        By productionTitle = By.xpath("//*[@id=\"item_4_title_link\"]/div");
-        WebElement productionElement = driver.findElement(productionTitle);
-        assertTrue(productionElement.isDisplayed());
-        assertEquals("Sauce Labs Backpack", productionElement.getText());
-    }
-
     @Then("user able to see error message {string}")
     public void userAbleToSeeErrorMessage(String errorMessage) {
-        assertTrue(driver.getPageSource().contains(errorMessage));
+        loginPage.validateErrorAppear(errorMessage);
     }
 }
